750d0461439abb7cc59b7aab4cc9cfba8b0df62d
tugi-os <tugrulozaltan@gmail.com>
2024-11-01 21:57:55 +0300

    Created contract.sol

---
+++ contract.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleTransfer {
+    // Para transferi yapılırken kullanılacak olay
+    event TransferMade(address indexed sender, address indexed receiver, uint256 amount);
+
+    // Kullanıcıdan diğerine para transferi yapma fonksiyonu
+    function transfer(address payable _receiver) public payable {
+        require(msg.value > 0, "Transfer miktarı 0'dan büyük olmalıdır.");
+        _receiver.transfer(msg.value); // Gönderilen miktarı alıcıya transfer et
+
+        emit TransferMade(msg.sender, _receiver, msg.value); // Transfer olayını kaydet
+    }
+}
